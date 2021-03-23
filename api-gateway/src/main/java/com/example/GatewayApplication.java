99dbd429df238e0a966ef52d385978617862f081
matthew <matthew@demo.com>
2021-03-23 22:48:17 +0800

    1

--- api-gateway/src/main/java/com/example/GatewayApplication.java
+++ api-gateway/src/main/java/com/example/GatewayApplication.java
@@ -1,8 +1,11 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-
-
+@SpringBootApplication
+@EnableZuulProxy
 public class GatewayApplication {
 
     public static void main(String[] args) {
